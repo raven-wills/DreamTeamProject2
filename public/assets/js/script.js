f983881a35d8bc3e6ae2ace88bccb62a5de9b257
Raven Wills <raven.wills@gmail.com>
2018-06-10 16:24:17 -0500

    FIXED BUGS

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,11 +1,9 @@
-  var myVar;
+function myFunction() {
+  setTimeout(showPage, 2500);
+}
 
-  function myFunction() {
-    myVar = setTimeout(showPage, 2500);
-  }
-
-  function showPage() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("loader-wrapper").style.display = "none";
-    document.getElementById("myDiv").style.display = "block";
-  }
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("loader-wrapper").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
