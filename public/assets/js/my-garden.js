a5f9aa496f334ff0ac539a011ff8879076616310
Raven Wills <raven.wills@gmail.com>
2018-06-14 23:57:08 -0500

    More Css

--- public/assets/js/my-garden.js
+++ public/assets/js/my-garden.js
@@ -1,11 +1,32 @@
+// Getting plant name
+// Adding an event listener for when the form is submitted
+$(".btn-floating").on("click", function() {
+  event.preventDefault();
 
-    // Getting plant name
-    // Adding an event listener for when the form is submitted
-    $(".btn-floating").on("click", function () {
+  // Post to new plant db
+  $.post("/api/my-garden", {
+    plant: $(this).attr("name")
+  });
+});
+
+jQuery(document).ready(function() {
+  if ($(".cd-stretchy-nav").length > 0) {
+    var stretchyNavs = $(".cd-stretchy-nav");
+
+    stretchyNavs.each(function() {
+      var stretchyNav = $(this),
+        stretchyNavTrigger = stretchyNav.find(".cd-nav-trigger");
+
+      stretchyNavTrigger.on("click", function(event) {
         event.preventDefault();
-
-        // Post to new plant db
-        $.post("/api/my-garden", {
-            plant: $(this).attr("name")
-        });
+        stretchyNav.toggleClass("nav-is-visible");
+      });
     });
+
+    $(document).on("click", function(event) {
+      !$(event.target).is(".cd-nav-trigger") &&
+        !$(event.target).is(".cd-nav-trigger span") &&
+        stretchyNavs.removeClass("nav-is-visible");
+    });
+  }
+});
