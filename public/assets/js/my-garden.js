11649d5b707d6915a76ed891407874bdd099873f
Kevin Pena <kevinpena160@gmail.com>
2018-06-15 09:30:27 -0500

    css

--- public/assets/js/my-garden.js
+++ public/assets/js/my-garden.js
@@ -55,34 +55,34 @@ $(window).scroll(function () {
 });
 
 
-// var $elementList = $('.row').find('div');
-// $('.typeahead').keyup(function (eve) {
+var $elementList = $('.row').find('div');
+$('.typeahead').keyup(function (eve) {
 
-//     searchString = $(this).val().toLowerCase();
-//     searchArray = searchString.split(' ');
-//     var len = searchArray.length;
-//     $elementList.each(function (index, elem) {
-//         $eleli = $(elem)
-//         $eleli.removeClass('hideThisLine');
-//         var oneLine = $eleli.text().toLowerCase()
-//         match = true,
-//             sal = len;
-//         while (sal--) {
-//             if (oneLine.indexOf(searchArray[sal]) == -1) {
-//                 match = false;
-//             }
-//         }
-//         if (!match) {
-//             //console.log('this one is gets hidden',searchString);
-//             $eleli.addClass('hideThisLine');
-//         }
-//     });
-//     $('.dontShow').removeClass('dontShow');
-//     $('.hideThisLine').addClass('dontShow');
-// });
-// $('#clearSearch').click(function (e) {
-//     $('#cBuscador').val('').keyup();
-// });
+    searchString = $(this).val().toLowerCase();
+    searchArray = searchString.split(' ');
+    var len = searchArray.length;
+    $elementList.each(function (index, elem) {
+        $eleli = $(elem)
+        $eleli.removeClass('hideThisLine');
+        var oneLine = $eleli.text().toLowerCase()
+        match = true,
+            sal = len;
+        while (sal--) {
+            if (oneLine.indexOf(searchArray[sal]) == -1) {
+                match = false;
+            }
+        }
+        if (!match) {
+            //console.log('this one is gets hidden',searchString);
+            $eleli.addClass('hideThisLine');
+        }
+    });
+    $('.dontShow').removeClass('dontShow');
+    $('.hideThisLine').addClass('dontShow');
+});
+$('#clearSearch').click(function (e) {
+    $('#cBuscador').val('').keyup();
+});
 
 
 function autocomplete(inp, arr) {
